dfe3a5260f5dad0c53fa02d234305559ffc77fcb
AanchalPandya <79903811+AanchalPandya@users.noreply.github.com>
2021-03-12 18:42:07 +0530

    Day 1 Assignment submitted

---
+++ Day 1 Assignment.py
@@ -0,0 +1,10 @@
+# Question 1
+x = int(input('Enter a value: '))
+y = int(input('Enter a value: '))
+print(x ** y)
+
+# Question 2
+li=[1,2,3,4,5,6,7,8,9,10]
+for i in li:
+  if i%3 == 0:
+   print(i)
